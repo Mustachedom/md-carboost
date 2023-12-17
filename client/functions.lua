ca29b4ae69bccdd9319ccb39607621358f8364f8
CFOUR <116594943+c-four77@users.noreply.github.com>
2023-12-17 21:48:09 +0100

    Update functions.lua
    fixed target option for buylaptop

--- client/functions.lua
+++ client/functions.lua
@@ -1,7 +1,6 @@
 local QBCore = exports['qb-core']:GetCoreObject()
 
-RegisterNetEvent("md-carboost:client:ChooseClass")
-AddEventHandler("md-carboost:client:ChooseClass", function() 
+RegisterNetEvent("md-carboost:client:ChooseClass", function() 
 lib.registerContext({
   id = 'carboosting',
   title = 'carboosting',
@@ -11,7 +10,7 @@ lib.registerContext({
       title = 'Car Boost Tier D',
       description = 'Boost A Tier D Car',
       event = "md-carboost:client:getcopsd",
-      arrow = true,
+      disabled = true,
     },
     {
       title = 'Car Boost Tier C',
@@ -51,80 +50,77 @@ end)
 
 
 ----------- functions
- function SpawnPed()
- local ped = GetEntityCoords(PlayerPedId())
- local current = "ig_priest"
-			RequestModel(current)
-			while not HasModelLoaded(current) do
-				Wait(0)
-			end
-			owner = CreatePed(26, current, ped.x + 10, ped.y -3 , ped.z - 1, 90.0, true, true)
-			NetworkRegisterEntityAsNetworked(owner)
-			networkID = NetworkGetNetworkIdFromEntity(owner)
-			SetNetworkIdCanMigrate(networkID, true)
-			SetNetworkIdExistsOnAllMachines(networkID, true)
-			SetPedRandomComponentVariation(owner)
-			SetPedRandomProps(owner)
-			SetEntityAsMissionEntity(owner)
-			SetEntityVisible(owner, true)
-			SetPedRelationshipGroupHash(owner)
-			SetPedAccuracy(owner)
-			SetPedArmour(owner)
-			SetPedCanSwitchWeapon(owner, true)
-			SetPedFleeAttributes(owner, false)
-			GiveWeaponToPed(owner, "weapon_pistol", 1, false, true)
-			TaskCombatPed(owner, PlayerPedId(), 0, 16)
-			SetPedCombatAttributes(owner, 46, true)
-			
+function SpawnPed()
+    local pedCoords = GetEntityCoords(cache.ped)
+    local current = 'ig_priest'
+    lib.requestModel(current)
+
+		owner = CreatePed(26, current, pedCoords.x + 10, pedCoords.y -3 , pedCoords.z - 1, 90.0, true, true)
+		NetworkRegisterEntityAsNetworked(owner)
+		networkID = NetworkGetNetworkIdFromEntity(owner)
+		SetNetworkIdCanMigrate(networkID, true)
+		SetNetworkIdExistsOnAllMachines(networkID, true)
+		SetPedRandomComponentVariation(owner)
+		SetPedRandomProps(owner)
+		SetEntityAsMissionEntity(owner)
+		SetEntityVisible(owner, true)
+		SetPedRelationshipGroupHash(owner)
+		SetPedAccuracy(owner)
+		SetPedArmour(owner)
+		SetPedCanSwitchWeapon(owner, true)
+		SetPedFleeAttributes(owner, false)
+		GiveWeaponToPed(owner, 'weapon_pistol', 1, false, true)
+		TaskCombatPed(owner, cache.ped, 0, 16)
+		SetPedCombatAttributes(owner, 46, true)
 end
 
+--2nd Peds
 function SpawnCarPedChase()
-local ped = GetEntityCoords(PlayerPedId())
-RequestModel(`faggio3`)
-				while not HasModelLoaded(`faggio3`) do
-					Wait(0)
-				end
-ClearAreaOfVehicles(ped.x, ped.y, ped.z, 15.0, false, false, false, false, false)
-transport = CreateVehicle(`faggio3`, ped.x+3, ped.y-2, ped.z-1, 52.0, true, true)
-SetEntityAsMissionEntity(transport)
---
-RequestModel("ig_priest")
-while not HasModelLoaded("ig_priest") do
-	Wait(10)
-end
-pilot = CreatePed(26, "ig_priest", ped.x, ped.y, ped.z, 268.9422, true, false)
-pilot2 = CreatePed(26, "ig_priest", ped.x, ped.y, ped.z, 268.9422, true, false)
-TaskCombatPed(pilot, PlayerPedId(), 0, 16)
-SetPedIntoVehicle(pilot, transport, -1)
-SetPedIntoVehicle(pilot2, transport, 0)
-SetPedFleeAttributes(pilot,false)
-SetPedCombatAttributes(pilot, 46, 1)
-SetPedCombatAbility(pilot, 100)
-SetPedCombatMovement(pilot, 2)
-SetPedCombatRange(pilot, 2)
-SetPedKeepTask(pilot, true)
-SetPedAsCop(pilot, true)
-TaskVehicleChase(pilot ,PlayerPedId() )
-GiveWeaponToPed(pilot2, "weapon_pistol", 1, false, true)
-TaskVehicleShootAtPed(pilot2, PlayerPedId())
+	local ped = GetEntityCoords(PlayerPedId())
+    	-- Motorcycle Model
+   	 local modelhash = `faggio3`
+    	lib.requestModel(modelhash)
+	ClearAreaOfVehicles(ped.x, ped.y, ped.z, 15.0, false, false, false, false, false)
+	transport = CreateVehicle(`faggio3`, ped.x+3, ped.y-2, ped.z-1, 52.0, true, true)
+	SetEntityAsMissionEntity(transport)
+   	 -- Pilot Peds
+    	local pilothash = 'ig_priest'
+    	lib.requestModel(pilothash)
+	pilot = CreatePed(26, pilothash, ped.x, ped.y, ped.z, 268.9422, true, false)
+	pilot2 = CreatePed(26, pilothash, ped.x, ped.y, ped.z, 268.9422, true, false)
+	TaskCombatPed(pilot, PlayerPedId(), 0, 16)
+	SetPedIntoVehicle(pilot, transport, -1)
+	SetPedIntoVehicle(pilot2, transport, 0)
+	SetPedFleeAttributes(pilot,false)
+	SetPedCombatAttributes(pilot, 46, 1)
+	SetPedCombatAbility(pilot, 100)
+	SetPedCombatMovement(pilot, 2)
+	SetPedCombatRange(pilot, 2)
+	SetPedKeepTask(pilot, true)
+	SetPedAsCop(pilot, true)
+	TaskVehicleChase(pilot ,PlayerPedId())
+	GiveWeaponToPed(pilot2, "weapon_pistol", 1, false, true)
+	TaskVehicleShootAtPed(pilot2, PlayerPedId())
 
 end
 
-CreateThread(function() 
-lib.requestModel("ig_priest", 1000)
-	local laptopsales = Config.LaptopSale
-	
-	 laptopseller = CreatePed(0, "ig_priest",laptopsales.x,laptopsales.y,laptopsales.z-1,  false, false)
-             FreezeEntityPosition(laptopseller, true)
-            SetEntityInvincible(laptopseller, true)
-local options = {
-			{
-						 name = 'laptopsales',
-						 icon = 'fa-solid fa-car',
-						label = 'Buy Laptop For $10k Cash',
-						event = 'md-carboost:client:buylaptop'
-							
-			}
-}
-exports.ox_target:addLocalEntity(laptopseller, options)
+CreateThread(function()
+    local modelHash = 'ig_priest'
+    lib.requestModel(modelHash, 1000)
+    local laptopSales = config.laptopSale
+    laptopSeller = CreatePed(0, modelHash, laptopSales.x, laptopSales.y, laptopSales.z -1, false, false)
+    FreezeEntityPosition(laptopSeller, true)
+    SetEntityInvincible(laptopSeller, true)
+
+      local options = {
+      {
+        name = 'laptopSales',
+        icon = 'fa-solid fa-car',
+        label = 'Buy Laptop For $10k Cash',
+        serverEvent = 'md-carboost:server:buylaptop'
+      }
+    }
+    exports.ox_target:addLocalEntity(laptopSeller, options)
 end)
+
+
